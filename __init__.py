89f0155e657f128348a87b7ab7d15b3cf58a2e49
nbassett <bassett.neil@gmail.com>
2021-04-19 16:05:29 -0600

    Added __init__.py to simplify imports

---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""
+File: ashes/__init__.py
+Author: Neil Bassett
+Date: 19 April 2021
+
+Description: imports important elements from various files.
+"""
+
+from coordinates import CTP_coordinates, EDGES_coordinates,\
+    REACH_coordinates
+from HorizonCalculator import HorizonCalculator
