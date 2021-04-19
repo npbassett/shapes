2c8cd7a082d90008a832f13b6e2bad182c0738d3
nbassett <bassett.neil@gmail.com>
2021-04-19 15:53:16 -0600

    Added coordinates.py file which contains locations of different 21-cm experiments.

---
+++ coordinates.py
@@ -0,0 +1,12 @@
+"""
+File: ashes/coordinates.py
+Author: Neil Bassett
+Date: 19 April 2021
+
+Description: File containing coordinates of several different global 21-cm
+             experiments in the form (longitude, latitude).
+"""
+
+CTP_coordinates = (-79.823038, 38.433727)
+EDGES_coordinates = (116.603480, -26.714923)
+REACH_coordinates = (21.375833, -30.833611)
