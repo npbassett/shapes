bfb16773738ff852747517abec00ddae3455ce77
nbassett <bassett.neil@gmail.com>
2021-04-20 13:16:02 -0600

    Added example script for the LunarHorizonCalculator class.

---
+++ examples/lunar_horizon_calculator.py
@@ -0,0 +1,24 @@
+"""
+File: ashes/examples/lunar_horizon_calculator.py
+Author: Neil Bassett
+Date: 20 April 2021
+
+Description: Example showing how to use the LunarHorizonCalculator
+             class.
+"""
+from ashes import LunarHorizonCalculator
+import matplotlib.pyplot as plt
+
+schrodinger_coordinates = (132.4, -75.0)
+lunar_horizon_calculator = LunarHorizonCalculator(schrodinger_coordinates)
+N_alpha = 361
+N_gamma = 1000
+horizon_azimuths, horizon_profile =\
+    lunar_horizon_calculator.horizon_profile(N_alpha, N_gamma)
+
+fig, ax = plt.subplots(figsize=(20, 5))
+plt.plot(horizon_azimuths, horizon_profile)
+plt.xlim(0, 360)
+plt.xlabel('Azimuth (deg)')
+plt.ylabel('Horizon Angle (deg)')
+plt.show()
