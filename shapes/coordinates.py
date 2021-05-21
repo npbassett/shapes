9e3881b15fb9d95590a6e5edabc5122500786dbe
nbassett <bassett.neil@gmail.com>
2021-05-21 14:45:02 -0600

    Updated coordinates file with more experiments.

--- shapes/coordinates.py
+++ shapes/coordinates.py
@@ -10,3 +10,6 @@ Description: File containing coordinates of several different global 21-cm
 CTP_coordinates = (-79.823038, 38.433727)
 EDGES_coordinates = (116.603480, -26.714923)
 REACH_coordinates = (21.375833, -30.833611)
+PRIZM_coordinates = (37.820044, -46.886820)
+SARAS_coordinates = (77.428282, 13.603118)
+MIST_coordinates = (-90.748944, 79.415194)
